b842d880d42336d569bf3d883b2e86c8067573e4
Gergely Nagy <preszthegas@gmail.com>
2019-09-25 11:00:24 +0200

    Changes

--- class/cserekulonbseg.cpp
+++ class/cserekulonbseg.cpp
@@ -6,13 +6,13 @@ int main() {
     int a = 1;
     int b = 2;
 
-    cout << "Csere elott A = "<<a<<" B = "<<b<<"\n";
+    cout << "Csere elott a = "<<a<<" b = "<<b<<"\n";
 
     a = a + b;  // a = 3
     b = a - b;  // b = 1
     a = a - b;  // a = 2
 
-    cout << "Csere utan A = "<<a<<" B = "<<b<<"\n";
+    cout << "Csere utan a = "<<a<<" b = "<<b<<"\n";
 
     return 0;
 }

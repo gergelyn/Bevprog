d201ebb9b90d7c878ecc0f96adfeddcee6ef7ef1
Gergely Nagy <preszthegas@gmail.com>
2019-11-25 17:55:20 +0100

    cheighteendrill Added

---
+++ drills/cheighteendrill/cheighteendrill_1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int ga[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+void f(int ai[], int n)
+{
+    int la[10] = {};
+    for (int i = 0; i < n; i++)
+        la[i] = ai[i];
+
+    cout << "Local copy\n";
+    for (const auto &a : la)
+        cout << a << ' ';
+    cout << '\n';
+
+    int *p = new int[n];
+    for (int i = 0; i < n; i++)
+        p[i] = ai[i];
+
+    cout << "Pointer to free store\n";
+    for (int i = 0; i < n; i++)
+        cout << p[i] << ' ';
+    cout << '\n';
+
+    delete[] p;
+}
+
+int fac(int n) { return n > 1 ? n * fac(n - 1) : 1; }
+
+int main()
+{
+    cout << "ga array\n";
+    f(ga, 10);
+
+    int aa[10] = {};
+    for (int i = 0; i < 10; i++)
+        aa[i] = fac(i + 1);
+
+    cout << "aa array\n";
+    f(aa, 10);
+}

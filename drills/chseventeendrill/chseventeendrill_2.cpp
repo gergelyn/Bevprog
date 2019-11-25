f4dc6e5fb3744b66e881278d33a9aad59800b23b
Gergely Nagy <preszthegas@gmail.com>
2019-11-25 17:14:11 +0100

    chseventeendrill Added

---
+++ drills/chseventeendrill/chseventeendrill_2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print_array(ostream &os, const int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        os << a[i] << '\n';
+        if (i == n - 1)
+            os << '\n';
+    }
+}
+
+void print_vector(ostream &os, const vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        os << v[i] << '\n';
+        if (i == v.size() - 1)
+            os << '\n';
+    }
+}
+
+int main()
+{
+    int *p1 = new int(7);
+    cout << p1 << '\n';
+    print_array(cout, p1, 1);
+
+    int *p2 = new int[7]{1, 2, 4, 8, 16, 32, 64};
+    cout << p2 << '\n';
+    print_array(cout, p2, 7);
+
+    int *p3 = p2;
+    p2 = p1;
+    p2 = p3;
+
+    cout << p1 << '\n';
+    print_array(cout, p1, 1);
+    cout << p2 << '\n';
+    print_array(cout, p2, 7);
+
+    delete p1;
+    delete[] p2;
+
+    p1 = new int[10];
+
+    for (int i = 1; i < 10; i++)
+    {
+        p1[0] = 1;
+        p1[i] = p1[i - 1] * 2;
+    }
+    print_array(cout, p1, 10);
+
+    p2 = new int[10];
+    p2 = p1;
+    print_array(cout, p2, 10);
+
+    vector<int> v(10);
+
+    for (int i = 1; i < 10; i++)
+    {
+        v[0] = 1;
+        v[i] = v[i - 1] * 2;
+    }
+    print_vector(cout, v);
+
+    vector<int> v1(10);
+    v1 = v;
+    print_vector(cout, v1);
+}
